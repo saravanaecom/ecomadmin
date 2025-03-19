05e1867525af8969c6bcb3faa641b03e120742bd
saravanaecom <saravanaecomweb@gmail.com>
2025-03-19 11:14:39 +0530

    Add files via upload

--- src/components/sucessmodel.jsx
+++ src/components/sucessmodel.jsx
@@ -17,7 +17,6 @@ const SuccessModal = ({ message, onClose }) => {
         <h3 className="text-2xl font-semibold text-green-600 text-center">Success</h3>
         <p className="text-green-500 mt-4 text-center text-lg">{message}</p>
         <div className="mt-6 flex justify-center">
-       
         </div>
       </div>
     </div>
